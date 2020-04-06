c69f882da6800a241ae047f4fa0842141876cd3c
rocio montero <monterorocios@gmail.com>
2020-04-06 20:44:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,7 @@
 def display_rainbow (colors)
+<<<<<<< HEAD
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors [6]}"
+=======
+  puts "colors: #{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[4]}, #{colors[5]}"
+>>>>>>> 70b04aaec66c9c702a0c8890003e8ab0efb30763
 end
